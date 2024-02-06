e62ea6c8479a5e37720eb5a909eee197b9f55567
oyhoyhk <oyhoyhk@naver.com>
2024-02-06 19:54:23 +0900

    change URL

--- app/constants.ts
+++ app/constants.ts
@@ -1,6 +1,6 @@
 export const API_KEY = "586e504f7868776136376646754264";
 export const EXPLANATION_API_URL =
-  "http://openapi.seoul.go.kr:8088/586e504f7868776136376646754264/xml/tvExplanationInfo/";
+  "//openapi.seoul.go.kr:8088/586e504f7868776136376646754264/xml/tvExplanationInfo/";
 
 export const NEWS_API_URL =
-  "http://openapi.seoul.go.kr:8088/586e504f7868776136376646754264/xml/tvReportedInfo/";
+  "//openapi.seoul.go.kr:8088/586e504f7868776136376646754264/xml/tvReportedInfo/";
